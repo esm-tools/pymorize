5a95bda33a254c010fe6d48904d72a57459b6b10
Paul Gierz <pgierz@awi.de>
2024-06-17 09:43:33 +0200

    doc: initial upload of documentation

---
+++ doc/conf.py
@@ -0,0 +1,78 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+import os
+import shutil
+import sys
+
+import sphinx.ext.apidoc
+
+sys.path.insert(0, os.path.abspath("../src"))
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = "pymorize"
+copyright = "2024, Paul Gierz"
+author = "Paul Gierz"
+
+
+modules = os.listdir("../src/")
+
+
+# Ensure the API folder exists:
+try:
+    os.makedirs("api")
+except FileExistsError:
+    shutil.rmtree("api")
+    os.makedirs("api")
+
+with open("API.rst", "w") as rst:
+    rst.write("==================\n")
+    rst.write("Code Documentation\n")
+    rst.write("==================\n")
+    rst.write(".. toctree::\n")
+    rst.write("   :glob:\n\n")
+    rst.write("   api/*")
+
+    for mod in sorted(modules):
+        sphinx.ext.apidoc.main(
+            [
+                "--no-toc",
+                "--module-first",
+                "--output-dir",
+                "api",
+                "../src/" + mod,
+            ]
+        )
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+# Add any Sphinx extension module names here, as strings. They can be
+# extensions coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
+extensions = [
+    "sphinx.ext.autodoc",
+    "sphinx.ext.autosectionlabel",
+    "sphinx.ext.graphviz",
+    "sphinx.ext.napoleon",
+    "sphinx.ext.viewcode",
+    "sphinx_copybutton",
+    "sphinx_rtd_theme",
+    "sphinx_tabs.tabs",
+    "sphinx_toolbox.collapse",
+]
+
+# Strip the input promps for code cells when copying
+copybutton_prompt_text = r">>> |\.\.\. |\$ |In \[\d*\]: | {2,5}\.\.\.: | {5,8}: "
+copybutton_prompt_is_regexp = True
+
+templates_path = ["_templates"]
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = "sphinx_rtd_theme"
+html_static_path = ["_static"]
---
+++ doc/installation.rst
@@ -0,0 +1,25 @@
+============
+Installation
+============
+
+The package can be installed in several ways:
+
+** From PyPI **
+
+This is the most straightforward way to install the package if you don't need to modify the source code. Just run::
+
+    pip install pymorize
+
+You can also install the latest version from the repository by running::
+
+    pip install git+https://github.com/pgierz/pymorize.git
+
+If you want to ensure an isolated install and make sure nothing conflicts with other packages you have, and you **do not want to change source code**, you can have a look at 
+`pipx <https://pipx.pypa.io/stable/>`_.
+
+** From source **
+
+If you want to modify the source code, you can install the package by cloning the repository and running::
+
+    git clone https://pgierz/pymorize.git
+    python setup.py install
