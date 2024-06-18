411000939dbd742eb1d025363cae12aeeb37a854
Paul Gierz <pgierz@awi.de>
2024-06-18 11:40:28 +0200

    ci: fixes python 3.12 setuptools dependency
    A bad fix to how I would like setuptools to actually work, since
    setuptools needed for setup.py to work properly. At some point, I need
    to learn how to do this correctly.

--- setup.py
+++ setup.py
@@ -16,6 +16,7 @@ def read(filename):
 
 setup(
     name="pymorize",
+    python_requires=">=3.9, <4",
     version=versioneer.get_version(),
     cmdclass=versioneer.get_cmdclass(),
     url="https://github.com/pgierz/pymorize",
@@ -61,12 +62,10 @@ setup(
         "Development Status :: 2 - Pre-Alpha",
         "License :: OSI Approved :: MIT License",
         "Programming Language :: Python",
-        "Programming Language :: Python :: 2",
-        "Programming Language :: Python :: 2.7",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.4",
-        "Programming Language :: Python :: 3.5",
-        "Programming Language :: Python :: 3.6",
-        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
     ],
 )
