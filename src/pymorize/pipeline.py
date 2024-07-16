3edf67664e2ba09062a6f51da33e2872d2be935f
Paul Gierz <pgierz@awi.de>
2024-07-16 12:39:24 +0200

    feat: add a basic CRUD DB for pipelines
    This implements a basic Create-Read-Update-Delete database for pipeline
    statuses. This can be used to check that status of a particular
    pipeline, so that it could potentially be revived after a killed job.
    
    Some caveats:
    * Python ID is used for each pipeline. I am not sure if this is a good
      idea since the ID might be unique for each run.
    
    The `create` is somewhat wrong, it could also update and is not strictly
    separated. Can be fixed later on.
    
    Tests auto-generated by GitHub, seem to cover at least the very basics.
    More sophisticated tests will come later on.
    
    Signed-off-by: Paul Gierz <pgierz@awi.de>

--- src/pymorize/pipeline.py
+++ src/pymorize/pipeline.py
@@ -2,17 +2,180 @@
 Pipeline of the data processing steps.
 """
 
+import json
+import os
+
+from loguru import logger
+
 from .utils import get_callable_by_name
+
+
+class PipelineDB:
+    """
+    This class provides a JSON-based database for managing pipeline data.
+    It provides CRUD (Create, Read, Update, Delete) operations for pipeline data.
+    The database is stored in a JSON file, with the filename based on the process ID and the ID of the pipeline object.
+
+    Attributes:
+    -----------
+    _db_file : str
+        The name of the file where the database is stored. Internal use only!
+    _db : dict
+        The in-memory representation of the database. Internal use only!
+
+    Methods:
+    --------
+    save():
+        Saves the in-memory database to a file.
+    load():
+        Loads the database from a file into memory.
+    create(step, data):
+        Creates a new entry in the database.
+    read(step):
+        Reads an entry from the database.
+    update(step, data):
+        Updates an entry in the database.
+    delete(step):
+        Deletes an entry from the database.
+    """
+
+    def __init__(self, pipeline):
+        """
+        Initializes the PipelineDB object.
+
+        Parameters:
+        -----------
+        pipeline: object
+            The pipeline object that this database is associated with.
+        """
+        pid = os.getpid()
+        self._db_file = f"pymorize_{pid}_pipeline_{id(pipeline)}.json"
+        self._db = {}
+
+    def save(self):
+        """
+        Saves the in-memory database to a file.
+        """
+        with open(self._db_file, "w") as f:
+            json.dump(self._db, f)
+
+    def load(self):
+        """
+        Loads the database from a file into memory.
+        If the file does not exist, initializes an empty database.
+        """
+        if os.path.exists(self._db_file):
+            with open(self._db_file, "r") as f:
+                self._db = json.load(f)
+        else:
+            self._db = {}
+
+    def create(self, step, data):
+        """
+        Creates a new entry in the database.
+
+        Parameters:
+        -----------
+        step: function
+            The step function.
+        data: dict
+            The data to be associated with the step.
+        """
+        self._db[step.__name__] = data
+
+    def read(self, step):
+        """
+        Reads an entry from the database.
+
+        Parameters:
+        -----------
+        step: function
+            The step function.
+
+        Returns:
+        --------
+        dict
+            The data associated with the step.
+        """
+        return self._db.get(step.__name__)
+
+    def update(self, step, data):
+        """
+        Updates an entry in the database.
+
+        Parameters:
+        -----------
+        step: function
+            The step function.
+        data: dict
+            The data to be updated.
+
+        Raises:
+        -------
+        KeyError: If the step is not found in the database.
+        """
+        step_dict = self._db[step.__name__]
+        step_dict.update(data)
+
+    def delete(self, step):
+        """
+        Deletes an entry from the database.
+
+        Parameters:
+        -----------
+        step: function
+            The step function.
+        """
+        del self._db[step.__name__]
+
+    def __enter__(self):
+        """
+        Loads the database from a file into memory when entering the context.
+        """
+        self.load()
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        """
+        Saves the in-memory database to a file when exiting the context.
+        """
+        self.save()
+
+    def __del__(self):
+        """
+        Removes the database file when the object is deleted if all steps have status "done".
+        """
+        if all(
+            step.get("status") == "done" for step in self._db.values()
+        ) and os.path.exists(self._db_file):
+            os.remove(self._db_file)
 
 
 class Pipeline:
     def __init__(self, *args):
         self.steps = args
+        self._db = PipelineDB(self)
 
     def run(self, data):
         for step in self.steps:
-            data = step(data)
+            with self._db as db:
+                if db.read(step).get("status") == "done":
+                    continue
+                else:
+                    self._start_step(step)
+                    data = step(data)
+                    self._end_step(step)
         return data
+
+    def _start_step(self, step):
+        logger.debug(f"Starting step: {step.__name__}")
+        with self._db as db:
+            db.create(step, {"status": "running"})
+
+    def _end_step(self, step):
+        logger.debug(f"Ending step: {step.__name__}")
+        with self._db as db:
+            db.update(step, {"status": "done"})
 
     @classmethod
     def from_list(cls, steps):
@@ -26,8 +189,6 @@ class Pipeline:
 class DefaultPipeline(Pipeline):
     def __init__(self):
         super().__init__(
+            # FIXME: Fill in with appropriate steps
             get_callable_by_name("pymorize.generic.load_data"),
-            get_callable_by_name("extract_feacitures"),
-            get_callable_by_name("train"),
-            get_callable_by_name("evaluate"),
         )
--- tests/test_pipeline.py
+++ tests/test_pipeline.py
@@ -1,11 +1,60 @@
-from pymorize.pipeline import Pipeline
+import os
+
+import pytest
+
+from pymorize.pipeline import Pipeline, PipelineDB
 
 
 def test_basic_creation():
-    pl = Pipeline()
+    Pipeline()
 
 
 def test_qualname_creation():
-    pl = Pipeline.from_qualname_list(
+    Pipeline.from_qualname_list(
         ["pymorize.generic.convert_units", "pymorize.generic.set_cmor_metadata"]
     )
+
+
+@pytest.fixture
+def pipeline_db():
+    return PipelineDB("test_pipeline")
+
+
+@pytest.fixture
+def step():
+    return lambda: None
+
+
+@pytest.fixture
+def data():
+    return {"key": "value"}
+
+
+def test_pipeline_db_init(pipeline_db):
+    assert pipeline_db._db == {}
+
+
+def test_pipeline_db_save_and_load(pipeline_db, step, data):
+    pipeline_db.create(step, data)
+    pipeline_db.save()
+    assert os.path.exists(pipeline_db._db_file)
+    pipeline_db._db = {}
+    pipeline_db.load()
+    assert pipeline_db._db == {step.__name__: data}
+
+
+def test_create_and_read(pipeline_db, step, data):
+    pipeline_db.create(step, data)
+    assert pipeline_db.read(step) == data
+
+
+def test_update(pipeline_db, step, data):
+    pipeline_db.create(step, data)
+    pipeline_db.update(step, {"new_key": "new_value"})
+    assert pipeline_db.read(step) == {"key": "value", "new_key": "new_value"}
+
+
+def test_delete(pipeline_db, step, data):
+    pipeline_db.create(step, data)
+    pipeline_db.delete(step)
+    assert pipeline_db.read(step) is None
