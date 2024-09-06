be208c77d777050265a67a6b4fed08f7a155d3ac
PavanSiligam <pavan.siligam@gmail.com>
2024-09-06 09:27:45 +0200

    fixed typos and refactored code

--- src/pymorize/webapp.py
+++ src/pymorize/webapp.py
@@ -5,20 +5,15 @@ import streamlit as st
 import pandas as pd
 
 
-tables_dir = Path(__file__).parent.parent.parent  / "cmip6-cmor-tables/Tables"
-tbls = defaultdict(list)
-ignored_tables = []
-var_to_tbl = defaultdict(list)
-frequencies = set()
-tids={}
-
 def process_table(tbl):
     add_to_ignore = False
     with open(tbl) as fid:
         t = json.load(fid)
     tid = t.get("Header", {}).get("table_id", "").replace("Table ", "")
     tids[tid] = tbl
-    if var_entry := t.get("variable_entry"):
+    if tid == "fx":
+        add_to_ignore = True
+    elif var_entry := t.get("variable_entry"):
         for name, attrs in var_entry.items():
             if freq := attrs.get("frequency"):
                 var_to_tbl[name].append((tid, freq))
@@ -34,35 +29,15 @@ def process_table(tbl):
     return
 
 
-for tbl in tables_dir.glob("*.json"):
-    process_table(tbl)
-
-
-cols = st.columns(3)
-
-with cols[0]:
-    st.metric("Tables", len(tbls))
-with cols[1]:
-    st.metric("Frequencies", len(frequencies))
-with cols[2]:
-    st.metric("Variables", len(var_to_tbl))
-
-
-with st.expander("Ignored tables"):
-    st.table(sorted(ignored_tables))
-
-st.divider()
-
-
 def show_selected_variable(varname):
     res = var_to_tbl[varname]
     kind = ""
     r = []
     for t, f in res:
         if f.endswith('Pt'):
-            kind = 'Instantanious'
+            kind = 'Instantaneous'
         elif f.endswith('C') or f.endswith('CM'):
-            kind = 'Climotology'
+            kind = 'Climatology'
         else:
             kind = 'Mean'
         r.append(dict(table=t, frequency=f, timemethod=kind))#, select=False))
@@ -90,12 +65,45 @@ def show_selected_variable(varname):
             def styler(row):
                 ncols = len(row)
                 if len(row.unique()) > 1:
-                    return ['background-color: #e8ebcf' for i in range(ncols)]
+                    return ['background-color: #eeecf4' for i in range(ncols)]
                 return ['background-color: white' for i in range(ncols)]
             if len(df_info.columns) > 1:
                 st.dataframe(df_info.style.apply(styler, axis=1), use_container_width=True)
             else:
                 st.dataframe(df_info, use_container_width=True)
+    return
+
+
+tables_dir = Path(__file__).parent.parent.parent  / "cmip6-cmor-tables/Tables"
+tbls = defaultdict(list)
+ignored_tables = []
+var_to_tbl = defaultdict(list)
+frequencies = set()
+tids={}
+
+for tbl in tables_dir.glob("*.json"):
+    process_table(tbl)
+
+st.markdown("""
+# Tables - Frequencies - Variables
+
+## Metrics
+""")
+
+cols = st.columns(3)
+
+with cols[0]:
+    st.metric("Tables", len(tbls))
+with cols[1]:
+    st.metric("Frequencies", len(frequencies))
+with cols[2]:
+    st.metric("Variables", len(var_to_tbl))
+
+
+with st.expander("Ignored tables"):
+    st.table(sorted(ignored_tables))
+
+st.divider()
 
 variables = sorted(var_to_tbl)
 
@@ -103,6 +111,8 @@ var_references = defaultdict(set)
 for vname, items in var_to_tbl.items():
     var_references[len(items)].add(vname)
 var_references = {counts: sorted(vnames) for counts, vnames in var_references.items()}
+
+st.markdown("## Variables")
 
 filtered_variables = st.checkbox("Filter variable list by number of references to tables")
 if filtered_variables:
