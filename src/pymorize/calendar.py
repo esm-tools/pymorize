352c54b3de76ad99c3717c7dba1c804f4c403d85
Paul Gierz <pgierz@awi.de>
2024-11-06 07:51:38 +0100

    fix/doc: incorrect call signature of examples in docstrings

--- src/pymorize/calendar.py
+++ src/pymorize/calendar.py
@@ -16,11 +16,11 @@ Examples
 >>> print(year_bounds)
 [[2000, 2001], [2002, 2003], [2004, 2005], [2006, 2007], [2008, 2009], [2010, 2010]]
 
->>> date_range = date_range_from_bounds(year_bounds, freq="Y")
+>>> date_range = date_ranges_from_bounds(year_bounds, freq="Y")
 >>> print(date_range)
 ([Timestamp('2000-12-31 00:00:00', freq='A-DEC'), Timestamp('2001-12-31 00:00:00', freq='A-DEC')], [Timestamp('2002-12-31 00:00:00', freq='A-DEC'), Timestamp('2003-12-31 00:00:00', freq='A-DEC')], [Timestamp('2004-12-31 00:00:00', freq='A-DEC'), Timestamp('2005-12-31 00:00:00', freq='A-DEC')], [Timestamp('2006-12-31 00:00:00', freq='A-DEC'), Timestamp('2007-12-31 00:00:00', freq='A-DEC')], [Timestamp('2008-12-31 00:00:00', freq='A-DEC'), Timestamp('2009-12-31 00:00:00', freq='A-DEC')], [Timestamp('2010-12-31 00:00:00', freq='A-DEC')])
 
->>> date_range = date_range_from_year_bounds(year_bounds, freq="Y")
+>>> date_range = date_ranges_from_year_bounds(year_bounds, freq="Y")
 >>> print(date_range)
 ([Timestamp('2000-12-31 00:00:00', freq='A-DEC'), Timestamp('2001-12-31 00:00:00', freq='A-DEC')], [Timestamp('2002-12-31 00:00:00', freq='A-DEC'), Timestamp('2003-12-31 00:00:00', freq='A-DEC')], [Timestamp('2004-12-31 00:00:00', freq='A-DEC'), Timestamp('2005-12-31 00:00:00', freq='A-DEC')], [Timestamp('2006-12-31 00:00:00', freq='A-DEC'), Timestamp('2007-12-31 00:00:00', freq='A-DEC')], [Timestamp('2008-12-31 00:00:00', freq='A-DEC'), Timestamp('2009-12-31 00:00:00', freq='A-DEC')], [Timestamp('2010-12-31 00:00:00', freq='A-DEC')])
 """
