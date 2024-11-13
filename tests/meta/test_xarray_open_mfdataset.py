e48436cb3f37c015e97555b88db3ada80c1c7905
Paul Gierz <pgierz@awi.de>
2024-11-13 15:53:03 +0100

    test: more explicit checks for open mfdataset

--- tests/meta/test_xarray_open_mfdataset.py
+++ tests/meta/test_xarray_open_mfdataset.py
@@ -2,9 +2,12 @@ import xarray as xr
 
 
 def test_open_fesom_2p6_pimesh_esm_tools(fesom_2p6_pimesh_esm_tools_data):
-    ds = xr.open_mfdataset(
+    matching_files = [
         f
         for f in (fesom_2p6_pimesh_esm_tools_data / "outdata/fesom/").iterdir()
-        if f.name.startswith("temp")
-    )
+        if f.name.startswith("temp.fesom")
+    ]
+    assert len(matching_files) > 0
+    print(matching_files)
+    ds = xr.open_mfdataset(matching_files)
     print(ds)
