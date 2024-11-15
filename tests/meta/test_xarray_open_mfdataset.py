427b04eb9b9914da79140253ef40f310299e5958
Paul Gierz <pgierz@awi.de>
2024-11-15 12:30:25 +0100

    wip

--- tests/meta/test_xarray_open_mfdataset.py
+++ tests/meta/test_xarray_open_mfdataset.py
@@ -8,3 +8,32 @@ def test_open_fesom_2p6_pimesh_esm_tools(fesom_2p6_pimesh_esm_tools_data):
         if f.name.startswith("temp")
     )
     assert isinstance(ds, xr.Dataset)
+
+
+def test_open_fesom_2p6_pimesh_esm_tools_parallel(fesom_2p6_pimesh_esm_tools_data):
+    ds = xr.open_mfdataset(
+        f
+        for f in (fesom_2p6_pimesh_esm_tools_data / "outdata/fesom/").iterdir()
+        if f.name.startswith("temp"),
+        parallel=True,
+    )
+    assert isinstance(ds, xr.Dataset)
+
+def test_open_fesom_2p6_pimesh_esm_tools_cftime(fesom_2p6_pimesh_esm_tools_data):
+    ds = xr.open_mfdataset(
+        f
+        for f in (fesom_2p6_pimesh_esm_tools_data / "outdata/fesom/").iterdir()
+        if f.name.startswith("temp"),
+        use_cftime=True,
+    )
+    assert isinstance(ds, xr.Dataset)
+
+def test_open_fesom_2p6_pimesh_esm_tools_full(fesom_2p6_pimesh_esm_tools_data):
+    ds = xr.open_mfdataset(
+        f
+        for f in (fesom_2p6_pimesh_esm_tools_data / "outdata/fesom/").iterdir()
+        if f.name.startswith("temp"),
+        use_cftime=True,
+        parallel=True,
+    )
+    assert isinstance(ds, xr.Dataset)
