d63794e4504523fa78289dc8652ad837b83671ec
Paul Gierz <pgierz@awi.de>
2024-11-15 12:32:35 +0100

    fix: bad paranthesis

--- tests/meta/test_xarray_open_mfdataset.py
+++ tests/meta/test_xarray_open_mfdataset.py
@@ -12,27 +12,35 @@ def test_open_fesom_2p6_pimesh_esm_tools(fesom_2p6_pimesh_esm_tools_data):
 
 def test_open_fesom_2p6_pimesh_esm_tools_parallel(fesom_2p6_pimesh_esm_tools_data):
     ds = xr.open_mfdataset(
-        f
-        for f in (fesom_2p6_pimesh_esm_tools_data / "outdata/fesom/").iterdir()
-        if f.name.startswith("temp"),
+        (
+            f
+            for f in (fesom_2p6_pimesh_esm_tools_data / "outdata/fesom/").iterdir()
+            if f.name.startswith("temp")
+        ),
         parallel=True,
     )
     assert isinstance(ds, xr.Dataset)
 
+
 def test_open_fesom_2p6_pimesh_esm_tools_cftime(fesom_2p6_pimesh_esm_tools_data):
     ds = xr.open_mfdataset(
-        f
-        for f in (fesom_2p6_pimesh_esm_tools_data / "outdata/fesom/").iterdir()
-        if f.name.startswith("temp"),
+        (
+            f
+            for f in (fesom_2p6_pimesh_esm_tools_data / "outdata/fesom/").iterdir()
+            if f.name.startswith("temp")
+        ),
         use_cftime=True,
     )
     assert isinstance(ds, xr.Dataset)
 
+
 def test_open_fesom_2p6_pimesh_esm_tools_full(fesom_2p6_pimesh_esm_tools_data):
     ds = xr.open_mfdataset(
-        f
-        for f in (fesom_2p6_pimesh_esm_tools_data / "outdata/fesom/").iterdir()
-        if f.name.startswith("temp"),
+        (
+            f
+            for f in (fesom_2p6_pimesh_esm_tools_data / "outdata/fesom/").iterdir()
+            if f.name.startswith("temp")
+        ),
         use_cftime=True,
         parallel=True,
     )
