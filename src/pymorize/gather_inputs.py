d840316470499669e02caa789cad743121865be4
Paul Gierz <pgierz@awi.de>
2024-11-18 14:59:11 +0100

    wip

--- src/pymorize/gather_inputs.py
+++ src/pymorize/gather_inputs.py
@@ -268,7 +268,7 @@ def load_mfdataset(data, rule_spec):
     rule_spec : Rule
         Rule being handled
     """
-    engine = rule_spec._pymorize_config("xarray_backend", "netcdf4")
+    engine = rule_spec._pymorize_config("xarray_backend")
     all_files = []
     for file_collection in rule_spec.inputs:
         for f in file_collection.files:
