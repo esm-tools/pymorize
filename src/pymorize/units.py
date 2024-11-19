da17269530653ba4c442bef924c22d17ea439e6f
Miguel Andres-Martinez <miguel.andres-martinez@awi.de>
2024-11-19 18:10:59 +0100

    add checker for missing required mappings, fix the test for the checker, and restructure handle_unit_conversion to have a better overview of the different steps on that function

--- src/pymorize/units.py
+++ src/pymorize/units.py
@@ -30,7 +30,6 @@ from .rule import Rule
 ureg = pint_xarray.unit_registry
 
 
-
 def handle_chemicals(
     s: Union[str, None] = None, pattern: Pattern = re.compile(r"mol(?P<symbol>\w+)")
 ):
@@ -95,19 +94,20 @@ def handle_unit_conversion(da: xr.DataArray, rule: Rule) -> xr.DataArray:
     """
     if not isinstance(da, xr.DataArray):
         raise TypeError(f"Expected xr.DataArray, got {type(da)}")
-    # data_request_variable needs to be defined at this point
+
     drv = rule.data_request_variable
-    to_unit = drv.unit
-    if not to_unit:
-        logger.error(
-            f"Unit of CMOR variable '{rule.cmor_variable}' not defined in the data request table/s {rule.tables}"
-        )
-        raise ValueError("Unit not defined")
+    dimless_mappings = rule.get("dimensionless_unit_mappings", {})
+
+    # Process model's unit (from_unit)
+    # --------------------------------
+    # (defined in the yaml file or in the original file)
     model_unit = rule.get("model_unit")
     from_unit = da.attrs.get("units")
+    # Overwrite model unit if defined in the yaml file
     if model_unit is not None:
         logger.info(
-            f"using user defined unit ({model_unit}) instead of ({from_unit}) from the original file"
+            f"using user defined unit ({model_unit}) instead of ({from_unit}) from the "
+            "original file"
         )
         from_unit = model_unit
     # Raise error if unit is not defined anywhere
@@ -118,34 +118,75 @@ def handle_unit_conversion(da: xr.DataArray, rule: Rule) -> xr.DataArray:
             f"rules.{rule.name}.model_unit"
         )
         raise ValueError("Unit not defined")
-    handle_chemicals(from_unit)
-    handle_chemicals(to_unit)
-    new_da = da.pint.quantify(from_unit)
-    dimless = rule.get("dimensionless_unit_mappings", {})
-    cmor_variable = rule.get("cmor_variable")
-    if cmor_variable in dimless:
-        _to_unit = dimless[cmor_variable][to_unit]
+
+    # Process table's unit (to_unit)
+    # ------------------------------
+    to_unit = drv.unit
+    cmor_variable_id = drv.variable_id
+    # Check for `to_unit` defined as `None`, `False`, empty string...
+    if not to_unit:
+        logger.error(
+            "Unit of CMOR variable '{cmor_variable_id}' not defined in the data "
+            f"request table/s {rule.tables}"
+        )
+        raise ValueError("Unit not defined")
+
+    # Check if the data request unit is a float
+    if unit_can_be_float(to_unit):
+        logger.debug(
+            f"Unit of CMOR variable '{cmor_variable_id}' can be a float: {to_unit}"
+        )
+        try:
+            _to_unit = dimless_mappings.get(cmor_variable_id, {})[to_unit]
+        except KeyError:
+            logger.error(
+                f"Dimensionless unit '{to_unit}' not found in mappings for "
+                f"CMOR variable '{cmor_variable_id}'"
+            )
+            raise KeyError("Dimensionless unit not found in mappings")
+        logger.info(
+            f"Converting units: ({da.name} -> {cmor_variable_id}) {from_unit} -> "
+            f"{to_unit}"
+        )
     else:
         _to_unit = to_unit
-    if _to_unit == to_unit:
         logger.info(
-            f"Converting units: ({da.name} -> {cmor_variable}) {from_unit} -> {to_unit}"
+            f"Converting units: ({da.name} -> {cmor_variable_id}) {from_unit} -> "
+            f"{_to_unit} ({to_unit})"
         )
-    else:
-        logger.info(
-            f"Converting units: ({da.name} -> {cmor_variable}) {from_unit} -> {_to_unit} ({to_unit})"
-        )
+
+    # Chemicals
+    # ---------
+    handle_chemicals(from_unit)
+    handle_chemicals(to_unit)
+
+    # Unit conversion
+    # ---------------
+    new_da = da.pint.quantify(from_unit)
     new_da = new_da.pint.to(_to_unit).pint.dequantify()
     if new_da.attrs.get("units") != to_unit:
         logger.debug(
-            "Pint auto-unit attribute setter different from requested unit string, setting manually."
+            "Pint auto-unit attribute setter different from requested unit string "
+            f"({new_da.attrs.get('units')} vs {to_unit}). Setting manually."
         )
         new_da.attrs["units"] = to_unit
     # Ensure a units attribute is present, default to None (this should never happen)
     if "units" not in new_da.attrs:
-        logger.warning(
-            "Units attribute not present in DataArray after conversion, please check carefully!"
+        logger.error("Units attribute not present in DataArray after conversion!")
+        raise AttributeError(
+            "Units attribute not present in DataArray after conversion!"
         )
-        logger.warning("Setting to None")
-        new_da.attrs["units"] = None
+
     return new_da
+
+
+def unit_can_be_float(value):
+    try:
+        _ = float(value)
+        return True
+    except ValueError as e:
+        logger.debug(f"unit_can_be_float: {e}")
+        return False
+    except TypeError as e:
+        logger.debug(f"unit_can_be_float: {e}")
+        return False
--- tests/unit/test_units.py
+++ tests/unit/test_units.py
@@ -178,7 +178,7 @@ def test_data_request_missing_unit(rule_with_data_request):
     """Test for missing unit attribute in the data request"""
     rule_spec = rule_with_data_request
     del rule_spec.data_request_variable.unit
-    da = xr.DataArray(10, name="var1", attrs={"units": None})
+    da = xr.DataArray(10, name="var1", attrs={"units": "kg m-2 s-1"})
 
     with pytest.raises(
         AttributeError, match="DataRequestVariable' object has no attribute 'unit'"
@@ -194,6 +194,17 @@ def test_data_request_not_defined_unit(rule_with_data_request):
 
     with pytest.raises(ValueError, match="Unit not defined"):
         new_da = handle_unit_conversion(da, rule_spec)
+
+
+def test_dimensionless_unit_missing_in_unit_mapping(rule_with_data_request):
+    """Test the checker for missing dimensionless unit in the unit mappings"""
+    rule_spec = rule_with_data_request
+    rule_spec.dimensionless_unit_mappings = {"var1": {"0.001": "g/kg"}}
+    rule_spec.data_request_variable.unit = "0.1"
+    da = xr.DataArray(10, name="var1", attrs={"units": "g/kg"})
+
+    with pytest.raises(KeyError, match="Dimensionless unit not found in mappings"):
+        handle_unit_conversion(da, rule_spec)
 
 
 def test_units_with_g_kg_to_0001_g_kg(rule_sos, CMIP_Tables_Dir):
@@ -228,13 +239,3 @@ def test_units_with_g_g_to_0001_g_kg(rule_sos, CMIP_Tables_Dir):
     assert new_da.attrs.get("units") == "0.001"
     # Check the magnitude of the data after conversion:
     assert np.equal(new_da.values, 10000)
-
-
-def test_dimensionless_unit_missing_in_unit_mapping(rule_with_data_request):
-    """Test the checker for missing dimensionless unit in the unit mappings"""
-    rule_spec = rule_with_data_request
-    rule_spec.data_request_variable.unit = "a_fake_unit"
-    da = xr.DataArray(10, name="sos", attrs={"units": "g/kg"})
-
-    with pytest.raises(KeyError):
-        handle_unit_conversion(da, rule_spec)
