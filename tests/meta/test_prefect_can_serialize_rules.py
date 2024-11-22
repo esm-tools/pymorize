99692187b0c0377933eaabed4e0819af27e2220f
Paul Gierz <pgierz@awi.de>
2024-11-22 10:06:35 +0100

    test: now with cache test

--- tests/meta/test_prefect_can_serialize_rules.py
+++ tests/meta/test_prefect_can_serialize_rules.py
@@ -1,9 +1,25 @@
-from prefect import task, flow
+from prefect import flow, task
+from prefect.cache_policies import INPUTS, TASK_SOURCE
 
 
 def test_prefect_can_serialize_rules(simple_rule):
 
     @task
+    def my_step(data, rule):
+        return data
+
+    @flow
+    def my_flow():
+        data_in = None
+        data_out = my_step(data_in, simple_rule)
+        return data_out
+
+    my_flow()
+
+
+def test_prefect_can_serialize_rules_with_cache(simple_rule):
+
+    @task(cache_policy=TASK_SOURCE + INPUTS)
     def my_step(data, rule):
         return data
 
