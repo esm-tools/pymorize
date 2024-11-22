33f2b758f6dc59abcb34025fb67f9c93df1dd832
Paul Gierz <pgierz@awi.de>
2024-11-22 10:42:32 +0100

    wip

--- tests/meta/test_prefect_can_serialize_rules.py
+++ tests/meta/test_prefect_can_serialize_rules.py
@@ -107,3 +107,43 @@ def test_prefect_can_serialize_as_pipeline_with_cache(simple_rule):
     pl = Pipeline()
     cmorizer = CMORizer([pl])
     cmorizer.run()
+
+
+def test_prefect_can_serialize_simplified():
+
+    class Pipeline:
+        @task
+        def my_step(self, data, rule):
+            return data
+
+        STEPS = [my_step]
+
+        @flow
+        def run(self, data, rule_spec):
+            for step in self.STEPS:
+                data = step(data, rule_spec)
+            return data
+
+    class CMORizer:
+        def __init__(self, rules, pipelines):
+            self.pipelines = pipelines
+            self.rules = rules
+
+        @flow
+        def run(self):
+            results = []
+            for rule in self.rules:
+                data = None
+                for pipeline in rule.pipelines:
+                    data = pipeline.run(data, rule)
+                results.append(data)
+            return results
+
+    class Rule:
+        def __init__(self, pipelines):
+            self.pipelines = pipelines
+
+    pl = Pipeline()
+    rule = Rule([pl])
+    cmorizer = CMORizer([rule], [pl])
+    cmorizer.run()
