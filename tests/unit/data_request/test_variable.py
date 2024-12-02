fe7eacf794581d6c66aae1b903a6786716182be5
Paul Gierz <pgierz@awi.de>
2024-12-02 15:50:14 +0100

    wip 123

--- tests/unit/data_request/test_variable.py
+++ tests/unit/data_request/test_variable.py
@@ -1,9 +1,15 @@
+"""
+Tests for DataRequestVariable
+"""
+
 from pymorize.data_request.variable import CMIP6JSONDataRequestVariable
 
 
 def test_init():
-    variable = CMIP6JSONDataRequestVariable.from_json_file(
+    drv = CMIP6JSONDataRequestVariable.from_json_file(
         "cmip6-cmor-tables/Tables/CMIP6_Omon.json",
         "thetao",
     )
-    assert variable.name == "thetao"
+    assert drv.name == "thetao"
+    assert drv.frequency == "mon"
+    assert drv.table_name == "Omon"
