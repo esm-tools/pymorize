554fea92a80062ebbe1c89fffff8d4ee4ad46071
Paul Gierz <pgierz@awi.de>
2024-12-04 14:56:44 +0100

    wip 123

--- tests/unit/test_global_attributes.py
+++ tests/unit/test_global_attributes.py
@@ -1,39 +1,54 @@
 from pymorize.global_attributes import set_global_attributes
 
-
+# Name, expected pass
 required_attributes = {
-    "activity_id",
-    "Conventions",
-    "creation_date",
-    "data_specs_version",
-    "experiment",
-    "experiment_id",
-    "forcing_index",
-    "frequency",
-    "further_info_url",
-    "grid",
-    "grid_label",
-    "initialization_index",
-    "institution",
-    "institution_id",
-    "license",
-    "mip_era",
-    "nominal_resolution",
-    "physics_index",
-    "product",
-    "realization_index",
-    "realm",
-    "source",
-    "source_id",
-    "source_type",
-    "sub_experiment",
-    "sub_experiment_id",
-    "table_id",
-    "tracking_id",
-    "variable_id",
-    "variant_label",
+    ("activity_id", True),
+    ("Conventions", True),
+    ("creation_date", True),
+    ("data_specs_version", True),
+    ("experiment", True),
+    ("experiment_id", True),
+    ("forcing_index", True),
+    ("frequency", True),
+    ("further_info_url", True),
+    ("grid", True),
+    ("grid_label", True),
+    ("initialization_index", True),
+    ("institution", True),
+    ("institution_id", True),
+    ("license", True),
+    ("mip_era", True),
+    ("nominal_resolution", True),
+    ("physics_index", True),
+    ("product", True),
+    ("realization_index", True),
+    ("realm", True),
+    ("source", True),
+    ("source_id", True),
+    ("source_type", True),
+    ("sub_experiment", True),
+    ("sub_experiment_id", True),
+    ("table_id", True),
+    ("tracking_id", False),
+    ("variable_id", True),
+    ("variant_label", True),
 }
 
 
-def test_global_attributes_has_expected_attributes():
-    pass
+@pytest.mark.parametrize("added_attributes, expected_pass", required_attributes)
+def test_global_attributes_has_expected_attributes(
+    pi_uxarray_temp_rule, pi_uxarray_data, added_attributes, expected_pass
+):
+    if not expected_pass:
+        pytest.xfail(f"Test should fail with attribute {added_attributes}")
+    matching_files = [
+        f
+        for f in (pi_uxarray_data / "outdata/fesom/").iterdir()
+        if f.name.startswith("temp.fesom")
+    ]
+    ds = xr.open_mfdataset(
+        matching_files,
+        engine="h5netcdf",
+    )
+    ds_out = set_global_attributes(ds, pi_uxarray_temp_rule)
+    assert added_attributes in ds_out.attrs
