2be4f67024728a1843407e6c0536dbbe2f78e640
Paul Gierz <pgierz@awi.de>
2024-12-11 08:32:23 +0100

    test: adds integration tests with real processing using CMIP7 data request backend

--- tests/fixtures/config_files.py
+++ tests/fixtures/config_files.py
@@ -19,5 +19,10 @@ def pi_uxarray_config():
 
 
 @pytest.fixture
+def pi_uxarray_config_cmip7():
+    return TEST_ROOT / "configs" / "test_config_pi_uxarray_cmip7.yaml"
+
+
+@pytest.fixture
 def fesom_2p6_pimesh_esm_tools_config():
     return TEST_ROOT / "configs" / "test_config_fesom_2p6_pimesh.yaml"
