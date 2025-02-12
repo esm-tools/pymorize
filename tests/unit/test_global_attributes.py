094a7768493c6d8fa9ab316461fd1c204a0347b7
PavanSiligam <pavan.siligam@gmail.com>
2025-02-12 12:59:23 +0100

    cleanup: removed commented (stale) code in test_global_attributes.py

--- tests/unit/test_global_attributes.py
+++ tests/unit/test_global_attributes.py
@@ -1,7 +1,5 @@
-import pytest
-
 from pymorize.global_attributes import GlobalAttributes
-from pymorize.cmorizer import CMORizer
+
 
 # Name, expected pass
 required_attributes = {
@@ -35,60 +33,6 @@ required_attributes = {
     ("variable_id", True),
     ("variant_label", True),
 }
-
-"""
-@pytest.mark.parametrize("added_attributes, expected_pass", required_attributes)
-def test_global_attributes_has_expected_attributes(
-    pi_uxarray_temp_rule, dr_sos, pi_uxarray_data, added_attributes, expected_pass
-):
-    if not expected_pass:
-        pytest.xfail(f"Test should fail with attribute {added_attributes}")
-    matching_files = [
-        f for f in (pi_uxarray_data).iterdir() if f.name.startswith("temp.fesom")
-    ]
-    ds = xr.open_mfdataset(
-        matching_files,
-        engine="h5netcdf",
-    )
-    test_rule = pi_uxarray_temp_rule
-    test_rule.data_request_variable = dr_sos
-    ds_out = set_global_attributes(ds, test_rule)
-    assert added_attributes in ds_out.attrs
-"""
-
-
-# @pytest.mark.parametrize("added_attributes, expected_pass", required_attributes)
-# def test_global_attributes_has_expected_attributes(added_attributes, expected_pass):
-#    class Fake:
-#        pass
-#
-#    ds = Fake()
-#    ds.attrs = {}
-#    rule = Fake()
-#    table_header = Fake()
-#    drv = Fake()
-#    header = {
-#        "table_id": "Omon",
-#        "mip_era": "CMIP6",
-#        "realm": "ocnBgchem",
-#        "Conventions": "CF-1.7 CMIP-6.2",
-#        "product": "model-output",
-#        "data_specs_version": "01.00.33",
-#    }
-#    for name, val in header.items():
-#        setattr(table_header, name, val)
-#
-#    drv.frequency = "mon"
-#    drv.table_header = table_header
-#    rule.data_request_variable = drv
-#    rule.cmor_variable = "fgco2"
-#    rule.variant_label = "r1i1p1f1"
-#    rule.source_id = "AWI-CM-1-1-MR"
-#    rule.experiment_id = "1pctCO2"
-#    rule.model_component = "atmos"
-#    rule.grid_label = "gn"
-#    ds_out = set_global_attributes(ds, rule)
-#    assert added_attributes in ds_out.attrs
 
 
 def test_global_attributes(rule_after_cmip6_cmorizer_init):
