451747a3b54f21f6e3e11be998e07ea6b2bc7726
Paul Gierz <pgierz@awi.de>
2025-04-01 19:53:12 +0200

    wip: removes leftovers from submodule

--- tests/unit/test_fesom.py
+++ tests/unit/test_fesom.py
@@ -2,19 +2,20 @@ import pymorize
 import pymorize.fesom_2p1.regridding
 
 
-def test_regridding(fesom_pi_mesh_config, fesom_pi_sst_ds):
+def test_regridding(fesom_pi_mesh_config, fesom_pi_sst_ds, pi_uxarray_mesh):
     config = fesom_pi_mesh_config
     rule = pymorize.rule.Rule.from_dict(config["rules"][0])
-    rule.mesh_path = config["inherit"]["mesh_path"]
+    rule.mesh_path = pi_uxarray_mesh
     da = fesom_pi_sst_ds.sst
     da = pymorize.fesom_2p1.regridding.regrid_to_regular(da, rule)
     assert da.shape == (180, 360)
 
 
-def test_attach_mesh_to_rule(fesom_pi_mesh_config):
+def test_attach_mesh_to_rule(fesom_pi_mesh_config, pi_uxarray_mesh):
     config = fesom_pi_mesh_config
     rule = pymorize.rule.Rule.from_dict(config["rules"][0])
-    rule.mesh_path = config["inherit"]["mesh_path"]
+    mesh_path = pi_uxarray_mesh
+    rule.mesh_path = mesh_path
     data = None  # Not important for this test
     assert not hasattr(rule, "mesh")
     # _ symbolizes just any return value, which we never use
