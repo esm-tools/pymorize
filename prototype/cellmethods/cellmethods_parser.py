a8acf44f7c3c87a2d0f1c88e6e24f98666600914
PavanSiligam <pavan.siligam@gmail.com>
2025-04-01 03:35:15 +0200

    refined parser to handle wider usecases. Refactored test cases aswell

--- prototype/cellmethods/cellmethods_parser.py
+++ prototype/cellmethods/cellmethods_parser.py
@@ -3,39 +3,70 @@ from sly import Lexer, Parser
 
 class CellMethodsLexer(Lexer):
     # set of token names
-    tokens = {DIMENSION, ACTION, REGION, CONSTRAINT, SCOPE, SELECTION}  # noqa: F821
+    tokens = {
+        DIMENSION,  # noqa: F821
+        FUNCTION,  # noqa: F821
+        CONSTRAINT,  # noqa: F821
+        AREATYPE,  # noqa: F821
+        SELECTION,  # noqa: F821
+        COMMENT,  # noqa: F821
+    }  # noqa: F821
 
     # string containing ignored characters between token
     ignore = " \t"
 
     # Regular expression rules for tokens
-    DIMENSION = r"area:|time:|grid_longitude:|longitude:|latitude:|depth:"
-    ACTION = r"mean|minimum|maximum|sum|point"
-    CONSTRAINT = r"within|over|where"  # Must come before REGION/SELECTION
-    REGION = r"[a-z_]+(?![a-z_])"  # Match words with underscores
-    SELECTION = r"[a-z_]+(?![a-z_])"  # Match words with underscores
+    # DIMENSION = r"area:|time:|grid_longitude:|longitude:|latitude:|depth:"
+    DIMENSION = r"[a-zA-Z_]+:"
+    FUNCTION = r"mean|minimum|maximum|sum|point"
+    CONSTRAINT = r"within|over|where"
+    AREATYPE = r"[a-zA-Z_]+"
+    SELECTION = r"[a-zA-Z_]+"
 
     def DIMENSION(self, t):  # noqa: F811
         t.value = t.value[:-1]
         return t
 
-    def REGION(self, t):  # noqa: F811
-        if t.value not in (
+    _areatypes = set(
+        [
             "land",
+            "shrubs",
+            "pastures",
+            "crops",
+            "trees",
+            "vegetation",
+            "unfrozen_soil",
+            "cloud",
+            "natural_grasses",
+            "floating_ice_shelf",
+            "grounded_ice_sheet",
+            "ice_free_sea",
+            "ice_sheet",
             "sea",
             "sea_ice",
+            "sea_ice_melt_pond",
+            "sea_ice_ridges",
             "snow",
-            "ice_sheet",
-            "grounded_ice_sheet",
-            "crops",
-            "ice_free_sea",
-        ):
+            "sector",
+        ]
+    )
+    _selection = set(["hours", "days", "years", "months"])
+    _selection.add("all_area_types")
+
+    @_(r"[a-zA-Z_]+")  # noqa: F821
+    def AREATYPE(self, t):  # noqa: F811
+        if t.value in self._areatypes:
+            return t
+        if t.value in self._selection:
             t.type = "SELECTION"
-        return t
+            return t
 
     @_(r"\(.*?\)")  # noqa: F821
-    def SCOPE(self, t):
-        t.value = t.value[1:-1]
+    def COMMENT(self, t):
+        value = t.value[1:-1]
+        t.value = (
+            value.replace("comment:", "").replace("[", "").replace("]", "").strip()
+        )
         return t
 
     @_(r"\n+")  # noqa: F821
@@ -45,13 +76,15 @@ class CellMethodsLexer(Lexer):
 
 class CellMethodsParser(Parser):
     tokens = CellMethodsLexer.tokens
+    debugfile = "parser.out"
 
     def __init__(self):
-        self.groups = []
+        self.tmp = []
 
     @_("statements")  # noqa: F821
     def program(self, p):
-        return self.groups
+        return corrections(p.statements)
+        # return p.statements
 
     @_("statement")  # noqa: F821
     def statements(self, p):
@@ -59,68 +92,67 @@ class CellMethodsParser(Parser):
 
     @_("statements statement")  # noqa: F821
     def statements(self, p):  # noqa: F811
-        return p.statements
+        return p.statements + p.statement
 
-    @_("dimension action")  # noqa: F821
+    @_("dimension function")  # noqa: F821
     def statement(self, p):
-        current_group = [("DIMENSION", p.dimension), ("ACTION", p.action)]
-        self.groups.append(current_group)
-        return p
+        return [p.dimension + p.function]
 
-    @_("dimension action SCOPE")  # noqa: F821
+    @_("dimension function comment")  # noqa: F821
     def statement(self, p):  # noqa: F811
-        current_group = [
-            ("DIMENSION", p.dimension),
-            ("ACTION", p.action),
-            ("SCOPE", p.SCOPE),
-        ]
-        self.groups.append(current_group)
-        return p
+        return [p.dimension + p.function + p.comment]
 
-    @_("dimension action constraint region")  # noqa: F821
+    @_("dimension function expr")  # noqa: F821
     def statement(self, p):  # noqa: F811
-        current_group = [
-            ("DIMENSION", p.dimension),
-            ("ACTION", p.action),
-            ("CONSTRAINT", p.constraint),
-            ("REGION", p.region),
-        ]
-        self.groups.append(current_group)
-        return p
+        return [p.dimension + p.function + p.expr]
 
-    @_("dimension action constraint region SCOPE")  # noqa: F821
+    @_("dimension function exprs")  # noqa: F821
     def statement(self, p):  # noqa: F811
-        current_group = [
-            ("DIMENSION", p.dimension),
-            ("ACTION", p.action),
-            ("CONSTRAINT", p.constraint),
-            ("REGION", p.region),
-            ("SCOPE", p.SCOPE),
-        ]
-        self.groups.append(current_group)
-        return p
+        return [p.dimension + p.function + p.exprs]
 
-    @_("dimension action constraint SELECTION")  # noqa: F821
+    @_("dimensions function")  # noqa: F821
     def statement(self, p):  # noqa: F811
-        current_group = [
-            ("DIMENSION", p.dimension),
-            ("ACTION", p.action),
-            ("CONSTRAINT", p.constraint),
-            ("SELECTION", p.SELECTION),
-        ]
-        self.groups.append(current_group)
-        return p
+        return [dim + p.function for dim in p.dimensions]
 
-    @_("dimensions ACTION")  # noqa: F821
+    @_("dimensions function comment")  # noqa: F821
     def statement(self, p):  # noqa: F811
-        for dimension in p.dimensions:
-            current_group = [("DIMENSION", dimension), ("ACTION", p.ACTION)]
-            self.groups.append(current_group)
-        return p
+        return [dim + p.function + p.comment for dim in p.dimensions]
 
-    @_("dimension")  # noqa: F821
+    @_("dimensions function expr")  # noqa: F821
+    def statement(self, p):  # noqa: F811
+        return [dim + p.function + p.expr for dim in p.dimensions]
+
+    @_("dimensions function exprs")  # noqa: F821
+    def statement(self, p):  # noqa: F811
+        return [dim + p.function + expr for dim in p.dimensions for expr in p.exprs]
+
+    @_("constraint areatype comment")  # noqa: F821
+    def expr(self, p):
+        return p.constraint + p.areatype + p.comment
+
+    @_("constraint selection comment")  # noqa: F821
+    def expr(self, p):  # noqa: F811
+        return p.constraint + p.selection + p.comment
+
+    @_("constraint areatype")  # noqa: F821
+    def expr(self, p):  # noqa: F811
+        return p.constraint + p.areatype
+
+    @_("constraint selection")  # noqa: F821
+    def expr(self, p):  # noqa: F811
+        return p.constraint + p.selection
+
+    @_("expr expr")  # noqa: F821
+    def exprs(self, p):
+        return p.expr0 + p.expr1
+
+    @_("exprs expr")  # noqa: F821
+    def exprs(self, p):  # noqa: F811
+        return p.exprs + p.expr
+
+    @_("dimension dimension")  # noqa: F821
     def dimensions(self, p):
-        return [p.dimension]
+        return [p.dimension0, p.dimension1]
 
     @_("dimensions dimension")  # noqa: F821
     def dimensions(self, p):  # noqa: F811
@@ -128,19 +160,64 @@ class CellMethodsParser(Parser):
 
     @_("DIMENSION")  # noqa: F821
     def dimension(self, p):
-        return p.DIMENSION
+        return [("DIMENSION", p.DIMENSION)]
 
-    @_("ACTION")  # noqa: F821
-    def action(self, p):
-        return p.ACTION
+    @_("FUNCTION")  # noqa: F821
+    def function(self, p):
+        return [("FUNCTION", p.FUNCTION)]
 
     @_("CONSTRAINT")  # noqa: F821
     def constraint(self, p):
-        return p.CONSTRAINT
+        return [("CONSTRAINT", p.CONSTRAINT)]
 
-    @_("REGION")  # noqa: F821
-    def region(self, p):
-        return p.REGION
+    @_("AREATYPE")  # noqa: F821
+    def areatype(self, p):
+        return [("AREATYPE", p.AREATYPE)]
+
+    @_("SELECTION")  # noqa: F821
+    def selection(self, p):
+        return [("SELECTION", p.SELECTION)]
+
+    @_("COMMENT")  # noqa: F821
+    def comment(self, p):
+        return [("COMMENT", p.COMMENT)]
+
+
+def corrections(groups):
+    result = []
+    for group in groups:
+        grp = []
+        tokens = iter(group)
+        tok = next(tokens)
+        tok_type, tok_value = tok
+        grp.append(tok)
+        if tok_type == "DIMENSION" and tok_value == "time":
+            while True:
+                try:
+                    tok = next(tokens)
+                except StopIteration:
+                    break
+                tok_type, tok_value = tok
+                # for `time` dimension, only SELECTION type is allowed as constraint
+                if tok_type == "AREATYPE":
+                    grp.pop()
+                else:
+                    grp.append(tok)
+        elif tok_type == "DIMENSION" and tok_value == "area":
+            while True:
+                try:
+                    tok = next(tokens)
+                except StopIteration:
+                    break
+                tok_type, tok_value = tok
+                if tok_type == "SELECTION" and tok_value != "all_area_types":
+                    grp.pop()
+                else:
+                    grp.append(tok)
+        else:
+            grp.extend(list(tokens))
+        result.append(grp)
+    return result
 
 
 class XArrayTranslator:
@@ -154,44 +231,49 @@ class XArrayTranslator:
 
     def translate_group(self, group):
         """Translate a single group of tokens into an xarray operation."""
-        tokens_dict = dict(group)
-
-        # Base operation
-        operation = f"{self.da_name}"
-
-        # Handle the main action
-        if "ACTION" in tokens_dict:
-            action = tokens_dict["ACTION"]
-            dim = tokens_dict.get("DIMENSION")
-
-            if "CONSTRAINT" in tokens_dict:
-                constraint = tokens_dict["CONSTRAINT"]
-                if constraint == "within":
-                    # For 'within', we first group by the selection
-                    selection = tokens_dict.get("SELECTION")
-                    if selection:
-                        operation = f"{operation}.groupby('{selection}').{action}()"
-                elif constraint == "over":
-                    # For 'over', we apply the operation over the selection
-                    selection = tokens_dict.get("SELECTION")
-                    if selection:
-                        operation = f"{operation}.{action}(dim='{selection}')"
-                elif constraint == "where":
-                    # For 'where', we apply a mask before the operation
-                    region = tokens_dict.get("REGION")
-                    if region:
-                        operation = (
-                            f"{operation}.where(mask=='{region}').{action}(dim='{dim}')"
-                        )
-            else:
-                # Simple dimension reduction
-                operation = f"{operation}.{action}(dim='{dim}')"
-
-            # Add any scope comments as a comment in the code
-            if "SCOPE" in tokens_dict:
-                operation = f"{operation}  # {tokens_dict['SCOPE']}"
-
-        return operation
+        tokens = iter(group)
+        token_type, dim = next(tokens)
+        assert token_type == "DIMENSION"
+        token_type, function = next(tokens)
+        assert token_type == "FUNCTION"
+        texts = []
+        try:
+            token_type, tok_value = next(tokens)
+        except StopIteration:
+            return f"{self.da_name}.{function}(dim={dim})"
+        else:
+            if token_type == "COMMENT":
+                if "mask=" in tok_value:
+                    mask = tok_value.split("=")[1]
+                    return f"{self.da_name}.where({mask}){function}(dim={dim}  # comment: {tok_value})"
+                else:
+                    return (
+                        f"{self.da_name}.{function}(dim={dim})  # comment: {tok_value}"
+                    )
+            elif token_type == "CONSTRAINT":
+                constraint = tok_value
+                token_type, tok_value = next(tokens)
+                text = f"{self.da_name}.{function}(dim={dim}).{constraint}({tok_value})"
+                texts.append(text)
+                if constraint == "over":
+                    token_type, tok_value = next(tokens)
+                    text = f"{self.da_name}.{function}(dim={dim}).{constraint}({tok_value})"
+                    texts.append(text)
+            while True:
+                try:
+                    token_type, tok_value = next(tokens)
+                except StopIteration:
+                    break
+                if token_type == "COMMENT":
+                    text = f"  # comment: {tok_value}"
+                    texts.append(text)
+                elif token_type == "CONSTRAINT":
+                    constraint = tok_value
+                    token_type, tok_value = next(tokens)
+                    text = f".{constraint}({tok_value})"
+                    texts.append(text)
+        text = "".join(texts)
+        return text
 
     def translate(self, groups):
         """Translate all groups into a sequence of xarray operations."""
@@ -217,10 +299,14 @@ class XArrayTranslator:
         return "\n".join(operations)
 
 
+lexer = CellMethodsLexer()
+parser = CellMethodsParser()
+
+
 def parse_cell_methods(text):
-    lexer = CellMethodsLexer()
-    parser = CellMethodsParser()
-    return parser.parse(lexer.tokenize(text))
+    tokens = lexer.tokenize(text)
+    group = parser.parse(tokens)
+    return group
 
 
 def translate_to_xarray(text):
@@ -229,15 +315,3 @@ def translate_to_xarray(text):
 
     parsed = parse_cell_methods(text)
     return translator.translate(parsed)
-
-
-if __name__ == "__main__":
-    text = "area: mean where sea depth: sum where sea (top 100m only) time: mean"
-    result = parse_cell_methods(text)
-    from pprint import pprint
-
-    pprint(result)
-    print(
-        "\nXArray translation (pseduo code, human readable strings. not xarray object):"
-    )
-    print(translate_to_xarray(text))
--- prototype/cellmethods/test_cellmethods_parser.py
+++ prototype/cellmethods/test_cellmethods_parser.py
@@ -1,201 +1,160 @@
-import pytest
-from cellmethods_parser import CellMethodsLexer, parse_cell_methods
-
-test_cases = [
-    (
-        "area: depth: time: mean",
-        [
-            [("DIMENSION", "area"), ("ACTION", "mean")],
-            [("DIMENSION", "depth"), ("ACTION", "mean")],
-            [("DIMENSION", "time"), ("ACTION", "mean")],
-        ],
-    ),
-    ("area: mean", [[("DIMENSION", "area"), ("ACTION", "mean")]]),
-    (
-        "area: mean (comment: over land and sea ice) time: point",
-        [
-            [
-                ("DIMENSION", "area"),
-                ("ACTION", "mean"),
-                ("SCOPE", "comment: over land and sea ice"),
-            ],
-            [("DIMENSION", "time"), ("ACTION", "point")],
-        ],
-    ),
-    (
-        "area: mean time: maximum",
-        [
-            [("DIMENSION", "area"), ("ACTION", "mean")],
-            [("DIMENSION", "time"), ("ACTION", "maximum")],
-        ],
-    ),
-    (
-        "area: mean time: maximum within days time: mean over days",
-        [
-            [("DIMENSION", "area"), ("ACTION", "mean")],
-            [
-                ("DIMENSION", "time"),
-                ("ACTION", "maximum"),
-                ("CONSTRAINT", "within"),
-                ("SELECTION", "days"),
-            ],
-            [
-                ("DIMENSION", "time"),
-                ("ACTION", "mean"),
-                ("CONSTRAINT", "over"),
-                ("SELECTION", "days"),
-            ],
-        ],
-    ),
-    (
-        "area: mean time: mean within days time: mean over days",
-        [
-            [("DIMENSION", "area"), ("ACTION", "mean")],
-            [
-                ("DIMENSION", "time"),
-                ("ACTION", "mean"),
-                ("CONSTRAINT", "within"),
-                ("SELECTION", "days"),
-            ],
-            [
-                ("DIMENSION", "time"),
-                ("ACTION", "mean"),
-                ("CONSTRAINT", "over"),
-                ("SELECTION", "days"),
-            ],
-        ],
-    ),
-    (
-        "area: mean time: mean within hours time: maximum over hours",
-        [
-            [("DIMENSION", "area"), ("ACTION", "mean")],
-            [
-                ("DIMENSION", "time"),
-                ("ACTION", "mean"),
-                ("CONSTRAINT", "within"),
-                ("SELECTION", "hours"),
-            ],
-            [
-                ("DIMENSION", "time"),
-                ("ACTION", "maximum"),
-                ("CONSTRAINT", "over"),
-                ("SELECTION", "hours"),
-            ],
-        ],
-    ),
-    (
-        "area: mean time: mean within years time: mean over years",
-        [
-            [("DIMENSION", "area"), ("ACTION", "mean")],
-            [
-                ("DIMENSION", "time"),
-                ("ACTION", "mean"),
-                ("CONSTRAINT", "within"),
-                ("SELECTION", "years"),
-            ],
-            [
-                ("DIMENSION", "time"),
-                ("ACTION", "mean"),
-                ("CONSTRAINT", "over"),
-                ("SELECTION", "years"),
-            ],
-        ],
-    ),
-    (
-        "area: mean time: minimum",
-        [
-            [("DIMENSION", "area"), ("ACTION", "mean")],
-            [("DIMENSION", "time"), ("ACTION", "minimum")],
-        ],
-    ),
-    (
-        "area: mean time: minimum within days time: mean over days",
-        [
-            [("DIMENSION", "area"), ("ACTION", "mean")],
-            [
-                ("DIMENSION", "time"),
-                ("ACTION", "minimum"),
-                ("CONSTRAINT", "within"),
-                ("SELECTION", "days"),
-            ],
-            [
-                ("DIMENSION", "time"),
-                ("ACTION", "mean"),
-                ("CONSTRAINT", "over"),
-                ("SELECTION", "days"),
-            ],
-        ],
-    ),
-    (
-        "area: mean time: point",
-        [
-            [("DIMENSION", "area"), ("ACTION", "mean")],
-            [("DIMENSION", "time"), ("ACTION", "point")],
-        ],
-    ),
-    (
-        "area: mean time: sum",
-        [
-            [("DIMENSION", "area"), ("ACTION", "mean")],
-            [("DIMENSION", "time"), ("ACTION", "sum")],
-        ],
-    ),
-]
+from cellmethods_parser import parse_cell_methods
 
 
-@pytest.mark.parametrize("input_text,expected_output", test_cases)
-def test_cell_methods_parser(input_text, expected_output):
-    result = parse_cell_methods(input_text)
-    assert (
-        result == expected_output
-    ), f"\nInput: {input_text}\nExpected: {expected_output}\nGot: {result}"
+def test_single_statement_with_just_action():
+    text = "area: mean"
+    result = parse_cell_methods(text)
+    expected = [[("DIMENSION", "area"), ("FUNCTION", "mean")]]
+    assert result == expected
 
 
-def test_lexer_tokens():
-    lexer = CellMethodsLexer()
-    # Test each token type is recognized correctly
-    test_tokens = {
-        "DIMENSION": [
-            "area:",
-            "time:",
-            "depth:",
-            "grid_longitude:",
-            "longitude:",
-            "latitude:",
+def test_single_statement_with_action_and_constraint():
+    text = "area: mean where land"
+    result = parse_cell_methods(text)
+    expected = [
+        [
+            ("DIMENSION", "area"),
+            ("FUNCTION", "mean"),
+            ("CONSTRAINT", "where"),
+            ("AREATYPE", "land"),
+        ]
+    ]
+    assert result == expected
+
+
+def test_single_statement_with_action_and_constraint_and_comment():
+    text = "area: mean where land (comment: mask=landFrac)"
+    result = parse_cell_methods(text)
+    expected = [
+        [
+            ("DIMENSION", "area"),
+            ("FUNCTION", "mean"),
+            ("CONSTRAINT", "where"),
+            ("AREATYPE", "land"),
+            ("COMMENT", "mask=landFrac"),
+        ]
+    ]
+    assert result == expected
+
+
+def test_many_dimensions_map_to_single_function():
+    text = "area: depth: time: mean"
+    result = parse_cell_methods(text)
+    expected = [
+        [
+            ("DIMENSION", "area"),
+            ("FUNCTION", "mean"),
         ],
-        "ACTION": ["mean", "minimum", "maximum", "sum", "point"],
-        "REGION": [
-            "land",
-            "sea",
-            "sea_ice",
-            "snow",
-            "ice_sheet",
-            "grounded_ice_sheet",
-            "crops",
-            "ice_free_sea",
+        [
+            ("DIMENSION", "depth"),
+            ("FUNCTION", "mean"),
         ],
-        "SELECTION": ["all_area_types", "days", "years", "hours"],
-        "CONSTRAINT": ["within", "over", "where"],
-        "SCOPE": ["(comment text)", "(top 100m only)"],
-    }
+        [
+            ("DIMENSION", "time"),
+            ("FUNCTION", "mean"),
+        ],
+    ]
+    assert result == expected
 
-    for token_type, values in test_tokens.items():
-        for value in values:
-            tokens = list(lexer.tokenize(value))
-            assert len(tokens) == 1, f"Expected 1 token for {value}, got {len(tokens)}"
-            token = tokens[0]
-            assert (
-                token.type == token_type
-            ), f"Expected token type {token_type} for {value}, got {token.type}"
-            if token_type == "DIMENSION":
-                assert (
-                    token.value == value[:-1]
-                ), f"Expected value {value[:-1]} for {value}, got {token.value}"
-            elif token_type == "SCOPE":
-                assert (
-                    token.value == value[1:-1]
-                ), f"Expected value {value[1:-1]} for {value}, got {token.value}"
-            else:
-                assert (
-                    token.value == value
-                ), f"Expected value {value}, got {token.value}"
+
+def test_statements_with_comment_in_middle():
+    text = "longitude: sum (comment: basin sum [along zig-zag grid path]) depth: sum time: mean"
+    result = parse_cell_methods(text)
+    expected = [
+        [
+            ("DIMENSION", "longitude"),
+            ("FUNCTION", "sum"),
+            ("COMMENT", "basin sum along zig-zag grid path"),
+        ],
+        [
+            ("DIMENSION", "depth"),
+            ("FUNCTION", "sum"),
+        ],
+        [
+            ("DIMENSION", "time"),
+            ("FUNCTION", "mean"),
+        ],
+    ]
+    assert result == expected
+
+
+def test_time_dimension_constraint_omits_areatpye():
+    text = "area: time: mean where cloud"
+    result = parse_cell_methods(text)
+    expected = [
+        [
+            ("DIMENSION", "area"),
+            ("FUNCTION", "mean"),
+            ("CONSTRAINT", "where"),
+            ("AREATYPE", "cloud"),
+        ],
+        [
+            ("DIMENSION", "time"),
+            ("FUNCTION", "mean"),
+        ],
+    ]
+    assert result == expected
+
+
+def test_multiple_contraints():
+    text = "area: mean where land over all_area_types time: mean"
+    result = parse_cell_methods(text)
+    expected = [
+        [
+            ("DIMENSION", "area"),
+            ("FUNCTION", "mean"),
+            ("CONSTRAINT", "where"),
+            ("AREATYPE", "land"),
+            ("CONSTRAINT", "over"),
+            ("SELECTION", "all_area_types"),
+        ],
+        [
+            ("DIMENSION", "time"),
+            ("FUNCTION", "mean"),
+        ],
+    ]
+    assert result == expected
+
+
+def test_statements_with_repeated_dimensions():
+    text = "area: mean where crops time: minimum within days time: mean over days"
+    result = parse_cell_methods(text)
+    expected = [
+        [
+            ("DIMENSION", "area"),
+            ("FUNCTION", "mean"),
+            ("CONSTRAINT", "where"),
+            ("AREATYPE", "crops"),
+        ],
+        [
+            ("DIMENSION", "time"),
+            ("FUNCTION", "minimum"),
+            ("CONSTRAINT", "within"),
+            ("SELECTION", "days"),
+        ],
+        [
+            ("DIMENSION", "time"),
+            ("FUNCTION", "mean"),
+            ("CONSTRAINT", "over"),
+            ("SELECTION", "days"),
+        ],
+    ]
+    assert result == expected
+
+
+def test_area_dimension_contraint_omits_selection():
+    text = "area: time: mean over days"
+    result = parse_cell_methods(text)
+    expected = [
+        [
+            ("DIMENSION", "area"),
+            ("FUNCTION", "mean"),
+        ],
+        [
+            ("DIMENSION", "time"),
+            ("FUNCTION", "mean"),
+            ("CONSTRAINT", "over"),
+            ("SELECTION", "days"),
+        ],
+    ]
+    assert result == expected
